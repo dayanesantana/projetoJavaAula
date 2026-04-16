8fc93e4aa36f13001666c274ed3f51d1474f483b
Dayane Santana <dayanesantana59@gmail.com>
2026-04-16 16:59:59 -0300

    Inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,13 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //metodo principal que executa o código
+		
+		//logica aqui
+		
+		System.out.print("Hello World"); //escreva
+
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,23 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		// VARIÁVEIS
+		
+		int idade = 25;
+		String nome = "Dayane"; //String precisa ser com S maiusculo
+		float graus = 20.5f;  //precisa do f para mostrar ao java que é do tipo float
+		boolean resposta; //true ou false
+		
+		//CONSTANTES - obrigatório usar o final antes e usar o nome em letras maiusculas
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		
+
+	}
+
+}
